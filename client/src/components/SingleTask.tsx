380d748eeb142d14348fa9a602327fd1561f8af5
SU-T <guitaristpeet@gmail.com>
2023-11-13 16:36:57 -0500

    passed task, allTask, and setAllTask props in the SingleTask component / specified their Props type

--- client/src/components/SingleTask.tsx
+++ client/src/components/SingleTask.tsx
@@ -1,4 +1,12 @@
-const SingleTask = () => {
+import { Task } from "../model";
+
+interface Props {
+  task: Task;
+  allTask: [Task];
+  setAllTask: React.Dispatch<React.SetStateAction<Task[]>>;
+}
+
+const SingleTask: React.FC<Props> = ({ task, allTask, setAllTask }) => {
   return <div>SingleTask</div>;
 };
 export default SingleTask;
